5aa4848362f08a520fd1ebae1acf1de7d817173b
RaykoRadev <christmas898@gmail.com>
2025-10-17 15:12:29 +0200

    adding catalog controller

--- Magma Haven/src/controllers/catalogController.js
+++ Magma Haven/src/controllers/catalogController.js
@@ -1,14 +1,22 @@
 import { Router } from "express";
-import { isGuest } from "../middlewares/authentication.js";
+import { isAuth, isGuest } from "../middlewares/authentication.js";
 
-const catalogcontroller = Router();
+const catalogController = Router();
 
-catalogcontroller.get("/create", isGuest, (req, res) => {
+catalogController.get("/create", isAuth, (req, res) => {
     res.render("catalog/create");
 });
 
-catalogcontroller.post("/create", isGuest, (req, res) => {
-    res.render("catalog/create");
+catalogController.post("/create", isAuth, (req, res) => {
+    const data = req.body;
+
+    try {
+    } catch (err) {
+        res.render("catalog/create", {
+            error: getErrorMessage(err),
+            catalog: data,
+        });
+    }
 });
 
-export default catalogcontroller;
+export default catalogController;
--- Magma Haven/src/routes.js
+++ Magma Haven/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import userController from "./controllers/userController.js";
+import catalogController from "./controllers/catalogController.js";
 
 const routes = Router();
 
@@ -7,6 +8,7 @@ routes.get("/", (req, res) => {
     res.render("home");
 });
 routes.use("/users", userController);
+routes.use("/catalog", catalogController);
 
 routes.get("*spat", (req, res) => {
     res.render("404");
