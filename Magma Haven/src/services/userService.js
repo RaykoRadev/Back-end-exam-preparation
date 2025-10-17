75655e8f0d6b07ed35de6deeb5e595db62145f3b
RaykoRadev <christmas898@gmail.com>
2025-10-17 13:01:48 +0200

    checking for existing user and passwords match

--- Magma Haven/src/services/userService.js
+++ Magma Haven/src/services/userService.js
@@ -2,6 +2,15 @@ import User from "../models/User.js";
 
 export default {
     async register(data) {
+        const user = await User.findOne({ email: data.email });
+        if (user) {
+            throw new Error("The user already exists!");
+        }
+
+        if (data.password !== data.rePassword) {
+            throw new Error("Passwords missmatch!");
+        }
+
         return User.create(data);
     },
 };
