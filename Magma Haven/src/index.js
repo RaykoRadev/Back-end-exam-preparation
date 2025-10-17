241eeffedcb1b6b9d0b88478bbeb38d1e4c4198c
RaykoRadev <christmas898@gmail.com>
2025-10-17 11:48:10 +0200

    install and setup handlebars

--- Magma Haven/src/index.js
+++ Magma Haven/src/index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+        runtimeOptions: {
+            allowProtoMethodsByDefault: true,
+            allowProtoPropertiesByDefault: true,
+        },
+    })
+);
+
+app.set("view engine", handlebars);
+app.set("views", "src/views");
 
 app.get("/", (req, res) => {
     res.send("it wokrks");
