c00b90669e46b3a0fa4714403aef50602b1ed5e2
RaykoRadev <christmas898@gmail.com>
2025-10-17 11:35:55 +0200

    install and setup express

---
+++ Magma Haven/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("it wokrks");
+});
+
+app.listen(
+    3000,
+    console.log("Server is listening on: http://localhost:3000...")
+);
