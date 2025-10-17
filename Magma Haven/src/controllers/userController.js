704c72a70050648e5c2b9556603377b93860edbf
RaykoRadev <christmas898@gmail.com>
2025-10-17 13:21:38 +0200

    adding loogout functionality

--- Magma Haven/src/controllers/userController.js
+++ Magma Haven/src/controllers/userController.js
@@ -28,4 +28,9 @@ userController.post("/register", async (req, res) => {
     }
 });
 
+userController.get("/logout", (req, res) => {
+    res.clearCookie("auth");
+    res.redirect("/");
+});
+
 export default userController;
