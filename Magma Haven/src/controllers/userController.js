09eec45bec48cc1ec977ef7e8fb4c3b0a4960d6f
RaykoRadev <christmas898@gmail.com>
2025-10-17 12:19:17 +0200

    adding user controller

---
+++ Magma Haven/src/controllers/userController.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const userController = Router();
+
+userController.get("register", async (req, res) => {
+    res.render("auth/register");
+});
+
+export default userController;
--- Magma Haven/src/routes.js
+++ Magma Haven/src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import userController from "./controllers/userController.js";
 
 const routes = Router();
+
+routes.use("/users", userController);
 
 routes.get("/", (req, res) => {
     res.render("home");
