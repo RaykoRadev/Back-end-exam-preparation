51591d4e0fe79620c52899a02c61cc41e387cc62
RaykoRadev <christmas898@gmail.com>
2025-10-18 13:48:57 +0200

    adding post model

---
+++ Wizard Creatures/src/model/Post.js
@@ -0,0 +1,52 @@
+import { Schema, Types, model, podel } from "mongoose";
+
+const postSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Post's name is required!"],
+        minLength: [2, "Post's name has to be at least 2 characters long!"],
+    },
+    species: {
+        type: String,
+        required: [true, "Post's spices is required!"],
+        minLength: [3, "Post's spices has to be at least 3 characters long!"],
+    },
+    skinColor: {
+        type: String,
+        required: [true, "Post's skin color is required!"],
+        minLength: [
+            3,
+            "Post's skin color has to be at least 3 characters long!",
+        ],
+    },
+    eyeColor: {
+        type: String,
+        required: [true, "Post's eye color is required!"],
+        minLength: [
+            3,
+            "Post's eye color has to be at least 3 characters long!",
+        ],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, "Post's image url is required!"],
+        match: [
+            /^(https?:\/\/).+/,
+            "Post's image url has to start with: http://... or https://...",
+        ],
+    },
+    description: {
+        type: String,
+        required: [true, "Post's description is required!"],
+        minLength: [
+            10,
+            "Post's description has to be at least 10 characters long!",
+        ],
+    },
+    votes: [{ type: Types.ObjectId, ref: "Auth" }],
+    owner: { type: Types.ObjectId, ref: "Auth" },
+});
+
+const Post = model("Post", postSchema);
+
+export default Post;
