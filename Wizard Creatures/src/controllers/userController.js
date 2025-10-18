74d9cf10338fef51f506406f08c1530c84306027
RaykoRadev <christmas898@gmail.com>
2025-10-18 12:11:46 +0200

    refactoring register page

--- Wizard Creatures/src/controllers/userController.js
+++ Wizard Creatures/src/controllers/userController.js
@@ -6,4 +6,10 @@ userController.get("/register", (req, res) => {
     res.render("auth/register");
 });
 
+userController.post("/register", async (req, res) => {
+    const userData = req.body;
+    console.log(userData);
+    res.render("auth/register", { user: userData });
+});
+
 export default userController;
