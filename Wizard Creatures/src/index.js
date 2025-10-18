c16f977b26a50fb74f1347c19d5e8776c99743c5
RaykoRadev <christmas898@gmail.com>
2025-10-18 11:45:21 +0200

    install and setup mongoose

--- Wizard Creatures/src/index.js
+++ Wizard Creatures/src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import handlebars from "express-handlebars";
+import mongoose from "mongoose";
+
 import routes from "./routes.js";
 import setTitle from "./utils/dynamicTitle.js";
 
 const app = express();
+const url = "mongodb://localhost:27017";
+
+try {
+    mongoose.connect(url, { dbName: "practising" });
+    console.log("Succssesful connected to DB");
+} catch (err) {
+    console.log("Cannot connect to DB");
+    console.log(err);
+}
 
 app.engine(
     "hbs",
