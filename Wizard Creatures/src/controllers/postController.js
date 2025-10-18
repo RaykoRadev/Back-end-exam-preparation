bec581d6e1dc3faed6158b60ef711af9ea05742f
RaykoRadev <christmas898@gmail.com>
2025-10-18 14:02:18 +0200

    addin create endpoint

--- Wizard Creatures/src/controllers/postController.js
+++ Wizard Creatures/src/controllers/postController.js
@@ -1,11 +1,29 @@
 import { Router } from "express";
 import postService from "../services/postService.js";
+import getErrorMessage from "../utils/errorhandler.js";
 
 const postController = Router();
 
 postController.get("/", async (req, res) => {
     const postArr = await postService.getAll();
     res.render("posts/all-posts", { post: postArr });
+});
+
+postController.get("/create", (req, res) => {
+    res.render("posts/create");
+});
+
+postController.post("/create", (req, res) => {
+    const postData = req.body;
+    try {
+        res.redirect("/creatures");
+    } catch (err) {
+        res.render("posts/create", {
+            error: getErrorMessage(err),
+            post: postData,
+        });
+    }
+    res.render("posts/create");
 });
 
 export default postController;
