387ee676d84b54a68e64dc932afabaf31f805860
RaykoRadev <christmas898@gmail.com>
2025-10-18 11:58:48 +0200

    adding users model

---
+++ Wizard Creatures/src/model/User.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, "User email is required!"],
+        minLength: [3, "User fisrt name has to be at least 3 characters!"],
+    },
+    lastName: {
+        type: String,
+        required: [true, "User last name is required!"],
+        minLength: [3, "User last name has to be at least 3 characters!"],
+    },
+    email: {
+        type: String,
+        required: [true, "User email is required!"],
+        minLength: [10, "User email has to be at least 10 characters!"],
+    },
+    password: {
+        type: String,
+        required: [true, "User password is required!"],
+        minLength: [4, "User password has to be at least 4 characters!"],
+    },
+});
+
+const User = model("User", userSchema);
+
+export default User;
