fa812954b7918e71fe29b187786296506d6f58c4
RaykoRadev <christmas898@gmail.com>
2025-10-18 11:24:46 +0200

    install and setup handlebars

--- Wizard Creatures/src/index.js
+++ Wizard Creatures/src/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import handlebars from "express-handlebars";
 import routes from "./routes.js";
 
 const app = express();
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+        runtimeOptions: {
+            allowProtoMethodsByDefault: true,
+            allowProtoPropertiesByDefault: true,
+        },
+    })
+);
+
+app.set("engine view", "hbs");
+app.set("views", "src/views");
 
 app.use(routes);
 
