3c85856077399a9746652871ceca21b3e52c6693
RaykoRadev <christmas898@gmail.com>
2025-10-23 13:10:36 +0200

    adding publish endpoint

--- Auction House/src/controllers/brouseController.js
+++ Auction House/src/controllers/brouseController.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { isAuth } from "../middlewares/authmiddleware.js";
 
 const brouseController = Router();
 
 brouseController.get("/", (req, res) => {
     res.render("auctions/browse");
+});
+
+brouseController.get("/publish", isAuth, (req, res) => {
+    res.render("auctions/publish");
 });
 
 export default brouseController;
