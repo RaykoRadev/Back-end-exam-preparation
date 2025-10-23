af3bee1cca31557863c55b756fcae46271443ff0
RaykoRadev <christmas898@gmail.com>
2025-10-23 11:59:15 +0200

    adding helper for votes count

--- Magma Haven/src/index.js
+++ Magma Haven/src/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import routes from "./routes.js";
 import setTilte from "./utils/pageHelper.js";
 import checkAuth from "./middlewares/authentication.js";
+import countHelper from "./utils/countHelper.js";
 
 const app = express();
 const url = "mongodb://localhost:27017";
@@ -34,7 +35,7 @@ app.engine(
             allowProtoMethodsByDefault: true,
             allowProtoPropertiesByDefault: true,
         },
-        helpers: { ...setTilte },
+        helpers: { ...setTilte, ...countHelper },
     })
 );
 
