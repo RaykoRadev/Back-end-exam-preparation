d455e9a9d2aba9b89cd5e320da91f9bbca955eb8
RaykoRadev <christmas898@gmail.com>
2025-10-24 10:36:37 +0200

    adding skeleton to the new project

---
+++ Art Gallery/src/controllers/userController.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { AUTH_COOKIE_NAME } from "../config/constants.js";
+import { isAuth, isGuest } from "../middlewares/authmiddleware.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
+import { userService } from "../services/index.js";
+
+const userController = Router();
+
+userController.get("/register", isGuest, (req, res) => {
+    res.render("user/register");
+});
+
+userController.post("/register", isGuest, async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await userService.register(userData);
+        res.cookie(AUTH_COOKIE_NAME, token);
+        res.redirect("/");
+    } catch (err) {
+        return res.status(404).render("user/register", {
+            error: getErrorMessage(err),
+            user: userData,
+        });
+    }
+});
+
+userController.get("/login", isGuest, (req, res) => {
+    res.render("user/login");
+});
+
+userController.post("/login", isGuest, async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await userService.login(userData);
+        res.cookie(AUTH_COOKIE_NAME, token);
+        res.redirect("/");
+    } catch (err) {
+        return res.status(404).render("user/login", {
+            error: getErrorMessage(err),
+            user: userData,
+        });
+    }
+});
+
+userController.get("/logout", isAuth, (req, res) => {
+    res.clearCookie(AUTH_COOKIE_NAME);
+    res.redirect("/");
+});
+
+export default userController;
---
+++ Art Gallery/src/helpers/arrayHelpers.js
@@ -0,0 +1,9 @@
+// todo adjust or delete if the bonus is not Profile page
+export default {
+    count(arr) {
+        return arr.length;
+    },
+};
+
+// use example in the hbs tamplate (Profile.hbs)
+// {{coint createdBlogs}}
